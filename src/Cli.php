88aedfcf7afe1cc789c563b048a74ed2559bb089
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2023-11-23 19:59:39 +0500

    feat: add autoload files, add Cli.php

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Php\Project\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function sayHello()
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt("May I have your name?");
+    line("Hello, %s!", $name);
+}
