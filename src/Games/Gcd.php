8a26e95ef527a70d834866fe059c8ece8b0bff21
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2023-11-25 17:18:50 +0500

    feat: add brain-gcd game

---
+++ src/Games/Gcd.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Php\Games\Gcd;
+
+require_once __DIR__ . '/../Engine.php';
+
+use function cli\line;
+use function cli\prompt;
+
+use const Engine\ROUNDS;
+use function Engine\sayHello;
+use function Engine\askQuestion;
+use function Engine\explainRules;
+use function Engine\getAnswer;
+use function Engine\checkAnswer;
+use function Engine\congratulate;
+
+function gcd ($a, $b) {
+    return $b ? gcd($b, $a % $b) : $a;
+}
+
+function playGcd()
+{
+    $name = sayHello();
+    $rule = 'Find the greatest common divisor of given numbers.';
+    explainRules($rule);
+    
+    $correctAnswersCount = 0;
+    
+    while ($correctAnswersCount < ROUNDS) {
+        $num1 = rand(0, 100);
+        $num2 = rand(0, 100);
+    
+        askQuestion("{$num1} {$num2}");
+        $correctAnswer = gcd($num1, $num2);   
+        $currentAnswer = getAnswer();
+    
+        if (checkAnswer($currentAnswer, $correctAnswer, $name)) {
+            $correctAnswersCount++;
+        } else {
+            return;
+        }
+    }
+    
+    congratulate($name);
+}
