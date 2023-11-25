2f9051e195e62dbc089b3cad6f7292098db169b9
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2023-11-25 15:49:53 +0500

    feat: add brain-calc

--- src/Engine.php
+++ src/Engine.php
@@ -16,6 +16,19 @@ use function cli\prompt;
 
 const ROUNDS = 3;
 
+function sayHello()
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt("May I have your name?", false, $marker = ' ');
+    line("Hello, %s!", $name);
+    return $name;
+}
+
+function explainRules($rules)
+{
+    line($rules);
+}
+
 function askQuestion($content)
 {
     line("Question: {$content}");
@@ -26,13 +39,19 @@ function getAnswer()
     return prompt("Your answer");
 }
 
-function checkAnswer(string $userAnswer, $correctAnswer)
+function checkAnswer(string $userAnswer, $correctAnswer, $name)
 {
-    if ($userAnswer === $correctAnswer) {
+    if ((string) $userAnswer === (string) $correctAnswer) {
         line('Correct!');
         return true;
     } else {
         line("{$userAnswer} is wrong answer ;(. Correct answer was {$correctAnswer}.");
+        line("Let's try again, {$name}!");
         return false;
     }
+}
+
+function congratulate($name)
+{
+    line("Congratulations, {$name}!");
 }
