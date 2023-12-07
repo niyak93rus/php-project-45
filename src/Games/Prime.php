47cfd75aec87f850a2d134c65c4483c00562fcb6
NYakovlev <n.yakovlev96@gmail.com>
2023-12-07 19:40:20 +0500

    fix: rewrite Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace App\Games\Prime;
 
-use function App\Engine\sayHello;
-use function App\Engine\askQuestion;
-use function App\Engine\explainRules;
-use function App\Engine\getAnswer;
-use function App\Engine\checkAnswer;
-use function App\Engine\congratulate;
-use function App\Engine\getRandomInt;
+use function App\Engine\playGame;
 
 use const App\Engine\ROUNDS;
 
@@ -31,21 +25,15 @@ function isPrime(int $num): bool
 
 function playPrime(): void
 {
-    $name = sayHello();
     $rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-    explainRules($rule);
+    $questions = [];
+    $correctAnswers = [];
 
     for ($i = 0; $i < ROUNDS; $i++) {
-        $number = getRandomInt();
-        askQuestion((string) $number);
-
-        $currentAnswer = getAnswer();
-        $correctAnswer = isPrime($number) ? 'yes' : 'no';
-
-        if (checkAnswer($currentAnswer, $correctAnswer, $name) === false) {
-            return;
-        }
+        $number = rand(0, 100);
+        $questions[] = ((string) $number);
+        $correctAnswers[] = isPrime($number) ? 'yes' : 'no';
     }
 
-    congratulate($name);
+    playGame($rule, $questions, $correctAnswers);
 }
