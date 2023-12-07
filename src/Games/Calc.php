6e437c29a70292ac9f0fd70414d924db27d6dd40
NYakovlev <n.yakovlev96@gmail.com>
2023-12-07 19:26:15 +0500

    fix: rewrite Calc.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -4,48 +4,36 @@ declare(strict_types=1);
 
 namespace App\Games\Calc;
 
-use function App\Engine\sayHello;
-use function App\Engine\getRandomInt;
-use function App\Engine\askQuestion;
-use function App\Engine\explainRules;
-use function App\Engine\getAnswer;
-use function App\Engine\checkAnswer;
-use function App\Engine\congratulate;
+use function App\Engine\playGame;
 
 use const App\Engine\ROUNDS;
 
 function playCalc(): void
 {
-    $name = sayHello();
-    $question = 'What is the result of the expression?';
-    explainRules($question);
+    $rule = 'What is the result of the expression?';
+    $questions = [];
+    $correctAnswers = [];
+    $operations = ['+', '-', '*'];
 
     for ($i = 0; $i < ROUNDS; $i++) {
-        $num1 = getRandomInt();
-        $num2 = getRandomInt();
-
-        $operations = ['+', '-', '*'];
+        $num1 = rand(0, 100);
+        $num2 = rand(0, 100);
+        
         $currentOperation = $operations[array_rand($operations)];
-        $content = "{$num1} {$currentOperation} {$num2}";
-        askQuestion($content);
-
+        $questions[] = "{$num1} {$currentOperation} {$num2}";
+    
         switch ($currentOperation) {
             case '+':
-                $correctAnswer = $num1 + $num2;
+                $correctAnswers[] = $num1 + $num2;
                 break;
             case '-':
-                $correctAnswer = $num1 - $num2;
+                $correctAnswers[] = $num1 - $num2;
                 break;
             case '*':
-                $correctAnswer = $num1 * $num2;
+                $correctAnswers[] = $num1 * $num2;
                 break;
         }
-
-        $currentAnswer = getAnswer();
-        if (checkAnswer($currentAnswer, $correctAnswer, $name) === false) {
-            return;
-        }
-    }
-
-    congratulate($name);
+    }    
+    
+    playGame($rule, $questions, $correctAnswers);
 }
