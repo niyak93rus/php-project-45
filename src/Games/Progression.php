43be440f14601c1c4c4b0365f87eadf67aaa0ab4
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2024-01-03 13:41:48 +0500

    feat: move game logic in Progression.php to separate function

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -13,26 +13,27 @@ const MIN_LENGTH = 5;
 const MAX_LENGTH = 10;
 const MIN_STEP = 1;
 
+function createGameData(): array
+{
+    $length = rand(MIN_LENGTH, MAX_LENGTH);
+    $step = rand(MIN_STEP, MAX_LENGTH);
+    $start = getRandomNumber();
+    $end = $start + $step * $length;
+    $numbers = range($start, $end, $step);
+    $missingItemKey = array_rand($numbers);
+    $correctAnswer = (string) $numbers[$missingItemKey];
+    $numbers[$missingItemKey] = '..';
+    $question = implode(' ', $numbers);
+    return [$question, $correctAnswer];
+}
+
 function playProgression(): void
 {
     $rule = 'What number is missing in the progression?';
     $gameData = [];
 
-    for ($i = 0; $i < ROUNDS; $i++) {
-        $length = rand(MIN_LENGTH, MAX_LENGTH);
-        $step = rand(MIN_STEP, MAX_LENGTH);
-        $start = getRandomNumber();
-        $end = $start + $step * $length;
-
-        $numbers = range($start, $end, $step);
-
-        $missingItemKey = array_rand($numbers);
-        $correctAnswer = (string) $numbers[$missingItemKey];
-        $numbers[$missingItemKey] = '..';
-        $question = implode(' ', $numbers);
-
-        $currentRoundData = [$question, $correctAnswer];
-        $gameData[] = $currentRoundData;
+    for ($i = 0; $i < ROUNDS; $i++) {      
+        $gameData[] = createGameData();
     }
 
     playGame($rule, $gameData);
